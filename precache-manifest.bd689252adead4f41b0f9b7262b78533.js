725f17f452ec23e3845e74588548606510d95c0a
Edward <e.arekhva@gmail.com>
2020-09-06 09:03:00 +0200

    Updates

---
+++ precache-manifest.bd689252adead4f41b0f9b7262b78533.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7419515d484cdd6625c677be335836e",
+    "url": "/Finance-app/index.html"
+  },
+  {
+    "revision": "f44fd252e0cd320285aa",
+    "url": "/Finance-app/static/css/main.d41691d1.chunk.css"
+  },
+  {
+    "revision": "003946afa0f97da9b9d8",
+    "url": "/Finance-app/static/js/2.441fb8f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Finance-app/static/js/2.441fb8f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f44fd252e0cd320285aa",
+    "url": "/Finance-app/static/js/main.c8ddcf0c.chunk.js"
+  },
+  {
+    "revision": "f0b231286e5f62266a0f",
+    "url": "/Finance-app/static/js/runtime-main.2947ca7e.js"
+  }
+]);
